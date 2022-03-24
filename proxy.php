7aec5aacddc7d416c818c2c0c9be8b8a0cf990ba
Andrea Cappalunga <andrea.cappalunga@gmail.com>
2022-03-24 01:32:03 +0100

    added proxy

---
+++ proxy.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$wsdl_url = "https://www.w3schools.com/xml/tempconvert.asmx?wsdl";
+
+$generator = new \Wsdl2PhpGenerator\Generator();
+$generator->generate(
+    new \Wsdl2PhpGenerator\Config(array(
+        'inputFile' => $wsdl_url,
+        'outputDir' => './output'
+    ))
+);
